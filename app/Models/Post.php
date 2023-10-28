f8d086782c0b0f1aac35f635a5c02960757f2967
FILIPE PETTER <fgpetter@gmail.com>
2023-10-28 16:29:16 -0300

    adiciona migrations models e controllers para posts no site

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Post extends Model
+{
+    use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [ 'titulo', 'slug', 'conteudo', 'thumb', 'rascunho','tipo' ];
+
+    /**
+     * Carrega categorias
+     * @return BelongsToMany
+     */
+    public function categorias() : BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
