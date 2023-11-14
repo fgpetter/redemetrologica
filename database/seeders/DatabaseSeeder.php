52ebdef01569411e0f0c94ac21aa981287be2569
FILIPE PETTER <fgpetter@gmail.com>
2023-11-13 23:37:06 -0300

    atualizacao das seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Pessoa::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Endereco::factory(10)->create();
+        \App\Models\Unidade::factory(10)->create();
     }
 }
