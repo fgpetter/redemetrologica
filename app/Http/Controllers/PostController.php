f54da8e28642641a7601a64d16ace1deba4d0009
Krausz90 <krausz90@gmail.com>
2023-11-23 19:41:31 -0300

    wic CMS

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,178 +8,246 @@ use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
-  /**
-   * Gera pagina de listagem de posts de noticias
-   *
-   * @return View
-   **/
-  public function indexNoticias(): View
-  {
-    $posts = Post::where('tipo', 'noticia')->get();
-    return view('post.noticia-index', ['posts' => $posts]);
-  }
-
-  /**
-   * Gera pagina de listagem de posts de galeria
-   *
-   * @return View
-   **/
-  public function indexGaleria(): View
-  {
-    $posts = Post::where('tipo', 'galeria')->get();
-    return view('post.noticia-index', ['posts' => $posts]);
-  }
-
-
-  /**
-   * Adiciona posts na base
-   *
-   * @param Request $request
-   * @return RedirectResponse
-   **/
-  public function create(Request $request): RedirectResponse
-  {
-    $request->validate(
-      [
-        'titulo' => ['required', 'string', 'max:255'],
-        'conteudo' => ['required', 'string'],
-        'thumb' => ['required', 'image', 'mimes:jpg,png,jpeg'],
-        'data_publicacao' => ['required', 'date'],
-      ],
-      [
-        'titulo.required' => 'Preencha o campo titulo',
-        'titulo.string' => 'O campo titulo tem caracteres inválidos',
-        'titulo.max' => 'O campo titulo aceita até 250 caracteres',
-        'conteudo.required' => 'Preencha o campo conteudo',
-        'conteudo.string' => 'O campo conteudo tem caracteres inválidos',
-        'data_publicacao.required' => 'Preencha o campo Data de publicação',
-        'data_publicacao.date' => 'Data de publicação invalida',
-      ]
-    );
-    if ($request->hasFile('thumb')) {
-        $originName = $request->file('thumb')->getClientOriginalName();
-        $fileName = pathinfo($originName, PATHINFO_FILENAME);
-        $extension = $request->file('thumb')->getClientOriginalExtension();
-        $fileName = $fileName . '_' . time() . '.' . $extension;
-        $request->file('thumb')->move(public_path('post-media'), $fileName);
-        $image = 'post-media/' . $fileName;
+    public $PastaTemp;
+    /**
+     * Gera pagina de listagem de posts de noticias
+     *
+     * @return View
+     **/
+    public function indexNoticias(): View
+    {
+        $posts = Post::where('tipo', 'noticia')->get();
+        return view('post.noticia-index', ['posts' => $posts]);
     }
 
-    $posts = Post::create([
-      'titulo' => $request->get('titulo'),
-      'slug' => Str::slug($request->get('titulo'), '-'),
-      'conteudo' => $request->get('conteudo'),
-      'thumb' => $image,
-      'rascunho' => $request->get('rascunho') ?? 0,
-      'tipo' => $request->get('tipo'),
-      'data_publicacao' => $request->get('data_publicacao')
-    ]);
-
-    if (!$posts) {
-      return redirect()->back()->withInput($request->input())->with('error', 'Ocorreu um erro!');
+    /**
+     * Gera pagina de listagem de posts de galeria
+     *
+     * @return View
+     **/
+    public function indexGaleria(): View
+    {
+        $posts = Post::where('tipo', 'galeria')->get();
+        return view('post.noticia-index', ['posts' => $posts]);
     }
 
-    if ($posts->tipo == 'noticia') {
-      return redirect()->route('noticia-index')->with('update-success', 'Noticia adicionada');
+    /**
+     * Salva imagem do form de conteúdo
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function storeImage(Request $request)
+    {
+        if ($request->hasFile('upload')) {
+            $originName = $request->file('upload')->getClientOriginalName();
+            $fileName = pathinfo($originName, PATHINFO_FILENAME);
+            $fileName = str_replace(' ', '_', $fileName);
+            $extension = $request->file('upload')->getClientOriginalExtension();
+            $fileName = $fileName . '_' . time() . '.' . $extension;
+
+            $this->PastaTemp = 'Temp' . substr(hrtime(true), -9, 9);
+            $request->file('upload')->move(public_path($this->PastaTemp), $fileName);
+            $request->session()->put('tempPasta', $this->PastaTemp);
+
+            $url = asset($this->PastaTemp  . '/' . $fileName);
+            return response()->json(['fileName' => $fileName, 'uploaded' => 1, 'url' => $url]);
+        }
     }
-    return redirect()->route('galeria-index')->with('update-success', 'Galeria adicionada');
-  }
+    /**
+     * Adiciona posts na base
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     **/
+    public function create(Request $request): RedirectResponse
+    {
+        $request->validate(
+            [
+                'titulo' => ['required', 'string', 'max:255'],
+                'conteudo' => ['required', 'string'],
+                'thumb' => ['required', 'image', 'mimes:jpg,png,jpeg'],
+                'data_publicacao' => ['required', 'date'],
+            ],
+            [
+                'titulo.required' => 'Preencha o campo titulo',
+                'titulo.string' => 'O campo titulo tem caracteres inválidos',
+                'titulo.max' => 'O campo titulo aceita até 250 caracteres',
+                'conteudo.required' => 'Preencha o campo conteudo',
+                'conteudo.string' => 'O campo conteudo tem caracteres inválidos',
+                'data_publicacao.required' => 'Preencha o campo Data de publicação',
+                'data_publicacao.date' => 'Data de publicação invalida',
+            ]
+        );
+        if ($request->hasFile('thumb')) {
+            $originName = $request->file('thumb')->getClientOriginalName();
+            $fileName = pathinfo($originName, PATHINFO_FILENAME);
+            $fileName = str_replace(' ', '-', $fileName);
+            $extension = $request->file('thumb')->getClientOriginalExtension();
+            $fileName = $fileName . '_' . time() . '.' . $extension;
+            $request->file('thumb')->move(public_path('post-media'), $fileName);
+            $image = 'post-media/' . $fileName;
+        }
+        if (session()->get('tempPasta') !== null) {
+            $this->PastaTemp = $request->session()->get('tempPasta');
+            //troca a pasta temporaria pela permanente
+            $conteudo = str_replace($this->PastaTemp, 'post-media', $request->get('conteudo'));
+            //movendo imagem e limpando pasta temporaria
 
-  /**
-   * Tela de edição de post
-   *
-   * @param Post $post
-   * @return View
-   **/
-  public function insert(Post $post): View
-  {
-    return view('post.insert', ['post' => $post]);
-  }
+            $tempMediaPath = public_path($this->PastaTemp);
+            $postMediaPath = public_path('post-media');
 
-  /**
-   * Edita dados de post
-   *
-   * @param Request $request
-   * @param Post $posts
-   * @return RedirectResponse
-   **/
-  public function update(Request $request, Post $post): RedirectResponse
-  {
+            $files = File::allFiles($tempMediaPath);
+            foreach ($files as $file) {
+                $destinationPath = $postMediaPath . DIRECTORY_SEPARATOR . $file->getFilename();
 
-    $request->validate(
-      [
-        'titulo' => ['required', 'string', 'max:255'],
-        'conteudo' => ['required', 'string'],
-        //'thumb' => ['required', 'image', 'mimes:jpg,png,jpeg'],
-        'data_publicacao' => ['required', 'date'],
-      ],
-      [
-        'titulo.required' => 'Preencha o campo titulo',
-        'titulo.string' => 'O campo titulo tem caracteres inválidos',
-        'titulo.max' => 'O campo titulo aceita até 250 caracteres',
-        'conteudo.required' => 'Preencha o campo conteudo',
-        'conteudo.string' => 'O campo conteudo tem caracteres inválidos',
-        'data_publicacao.required' => 'Preencha o campo Data de publicação',
-        'data_publicacao.date' => 'Data de publicação invalida',
+                // copia se o arquivo não existir
+                if (!File::exists($destinationPath)) {
+                    File::copy($file->getPathname(), $destinationPath);
+                }
+            }
+            //deleta pasta temporaria
+            File::deleteDirectory($tempMediaPath);
+            //limpa a session
+            $request->session()->forget('tempPasta');
+        }
 
-      ]
-    );
-    $post->update([
-      'titulo' => $request->get('titulo'),
-      'slug' => Str::slug($request->get('titulo'), '-'),
-      'conteudo' => $request->get('conteudo'),
-      'thumb' => $request->get('thumb'),
-      'rascunho' => $request->get('rascunho') ?? 0,
-      'tipo' => $request->get('tipo'),
-      'data_publicacao' => $request->get('data_publicacao')
-    ]);
 
-    if (!$post) {
-      return redirect()->back()->withInput($request->input())->with('error', 'Ocorreu um erro!');
+        $posts = Post::create([
+            'titulo' => $request->get('titulo'),
+            'slug' => Str::slug($request->get('titulo'), '-'),
+            'conteudo' => $conteudo ?? $request->get('conteudo'),
+            'thumb' => $image ?? $request->get('thumb'),
+            'rascunho' => $request->get('rascunho') ?? 0,
+            'tipo' => $request->get('tipo'),
+            'data_publicacao' => $request->get('data_publicacao')
+        ]);
+
+        if (!$posts) {
+            return redirect()->back()->withInput($request->input())->with('error', 'Ocorreu um erro!');
+        }
+
+        if ($posts->tipo == 'noticia') {
+            return redirect()->route('noticia-index')->with('update-success', 'Noticia adicionada');
+        }
+        return redirect()->route('galeria-index')->with('update-success', 'Galeria adicionada');
     }
 
-    if ($post->tipo == 'noticia') {
-      return redirect()->route('noticia-index')->with('update-success', 'Noticia atualizada');
+    /**
+     * Tela de edição de post
+     *
+     * @param Post $post
+     * @return View
+     **/
+    public function insert(Post $post): View
+    {
+
+        return view('post.insert', ['post' => $post]);
     }
 
-    return redirect()->route('galeria-index')->with('update-success', 'Galeria atualizada');
-  }
+    /**
+     * Edita dados de post
+     *
+     * @param Request $request
+     * @param Post $posts
+     * @return RedirectResponse
+     **/
+    public function update(Request $request, Post $post): RedirectResponse
+    {
 
-  /**
-   * Remove post
-   *
-   * @param Request $request
-   * @param User $post
-   * @return RedirectResponse
-   **/
-  public function delete(Request $request, Post $post): RedirectResponse
-  {
-    $post->delete();
+        $request->validate(
+            [
+                'titulo' => ['required', 'string', 'max:255'],
+                'conteudo' => ['required', 'string'],
+                // 'thumb' => ['required', 'image', 'mimes:jpg,png,jpeg'],
+                'data_publicacao' => ['required', 'date'],
+            ],
+            [
+                'titulo.required' => 'Preencha o campo titulo',
+                'titulo.string' => 'O campo titulo tem caracteres inválidos',
+                'titulo.max' => 'O campo titulo aceita até 250 caracteres',
+                'conteudo.required' => 'Preencha o campo conteudo',
+                'conteudo.string' => 'O campo conteudo tem caracteres inválidos',
+                'data_publicacao.required' => 'Preencha o campo Data de publicação',
+                'data_publicacao.date' => 'Data de publicação invalida',
+            ]
+        );
+        if ($request->hasFile('thumb')) {
+            $originName = $request->file('thumb')->getClientOriginalName();
+            $fileName = pathinfo($originName, PATHINFO_FILENAME);
+            $fileName = str_replace(' ', '-', $fileName);
+            $extension = $request->file('thumb')->getClientOriginalExtension();
+            $fileName = $fileName . '_' . time() . '.' . $extension;
+            $request->file('thumb')->move(public_path('post-media'), $fileName);
+            $image = 'post-media/' . $fileName;
+            // deleta arquivo anterior
+            if (File::exists(public_path($post->thumb))) {
+                File::delete(public_path($post->thumb));
+            }
+        } else {
+            $image = $post->thumb;
+        }
 
-    return redirect()->route('noticia-index')->with('update-success', 'Post removido');;
-  }
+        if (session()->get('tempPasta') !== null) {
+            $this->PastaTemp = $request->session()->get('tempPasta');
+            //troca a pasta temporaria pela permanente
+            $conteudo = str_replace($this->PastaTemp, 'post-media', $request->get('conteudo'));
+            //movendo imagem e limpando pasta temporaria
 
-  /**
-   * Salva imagem do form de conteúdo
-   *
-   * @param Request $request
-   * @return Response
-   */
-  public function storeImage(Request $request)
-  {
-    if ($request->hasFile('upload')) {
-      $originName = $request->file('upload')->getClientOriginalName();
-      $fileName = pathinfo($originName, PATHINFO_FILENAME);
-      $extension = $request->file('upload')->getClientOriginalExtension();
-      $fileName = $fileName . '_' . time() . '.' . $extension;
+            $tempMediaPath = public_path($this->PastaTemp);
+            $postMediaPath = public_path('post-media');
 
-      $request->file('upload')->move(public_path('post-media'), $fileName);
+            $files = File::allFiles($tempMediaPath);
+            foreach ($files as $file) {
+                $destinationPath = $postMediaPath . DIRECTORY_SEPARATOR . $file->getFilename();
 
-      $url = asset('post-media/' . $fileName);
-      return response()->json(['fileName' => $fileName, 'uploaded' => 1, 'url' => $url]);
+                // copia se o arquivo não existir
+                if (!File::exists($destinationPath)) {
+                    File::copy($file->getPathname(), $destinationPath);
+                }
+            }
+            //deleta pasta temporaria
+            File::deleteDirectory($tempMediaPath);
+            //limpa a session
+            $request->session()->forget('tempPasta');
+        }
+
+        $post->update([
+            'titulo' => $request->get('titulo'),
+            'slug' => Str::slug($request->get('titulo'), '-'),
+            'conteudo' => $conteudo ?? $request->get('conteudo'),
+            'thumb' => $image ?? $request->get('thumb'),
+            'rascunho' => $request->get('rascunho') ?? 0,
+            'tipo' => $request->get('tipo'),
+            'data_publicacao' => $request->get('data_publicacao')
+        ]);
+
+        if (!$post) {
+            return redirect()->back()->withInput($request->input())->with('error', 'Ocorreu um erro!');
+        }
+
+        if ($post->tipo == 'noticia') {
+            return redirect()->route('noticia-index')->with('update-success', 'Noticia atualizada');
+        }
+
+        return redirect()->route('galeria-index')->with('update-success', 'Galeria atualizada');
     }
-  }
+
+    /**
+     * Remove post
+     *
+     * @param Request $request
+     * @param User $post
+     * @return RedirectResponse
+     **/
+    public function delete(Request $request, Post $post): RedirectResponse
+    {
+        $post->delete();
+
+        return redirect()->route('noticia-index')->with('update-success', 'Post removido');;
+    }
 }
--- resources/views/components/painel/noticias/insert.blade.php
+++ resources/views/components/painel/noticias/insert.blade.php
@@ -1,112 +1,121 @@
 @if (session('error'))
-  <div class="alert alert-danger"> {{ session('error') }} </div>
+    <div class="alert alert-danger"> {{ session('error') }} </div>
 @endif
 @if (session('success'))
-  <div class="alert alert-success"> {{ session('success') }} </div>
+    <div class="alert alert-success"> {{ session('success') }} </div>
 @endif
 <div class="card">
-  <div class="card-body">
-    <div class="tab-content">
-      <div class="tab-pane active" id="post" role="tabpanel">
-        <form method="POST" enctype="multipart/form-data"
-          action="{{ isset($post->slug) ? route('post-update', $post->slug) : route('post-create') }}">
-          @csrf
-          <div class="row gy-3">
+    <div class="card-body">
+        <div class="tab-content">
+            <div class="tab-pane active" id="post" role="tabpanel">
+                <form method="POST" enctype="multipart/form-data"
+                    action="{{ isset($post->slug) ? route('post-update', $post->slug) : route('post-create') }}">
+                    @csrf
+                    <div class="row gy-3">
 
-            <div class="col-12">
-              <label class="form-label">Titulo
-                <small class="text-danger-emphasis opacity-75">(Obrigatório) </small>
-              </label>
-              <input type="text" class="form-control" name="titulo"
-                value="{{ old('titulo') ?? ($post->titulo ?? null) }}">
-              @error('titulo')
-                <div class="text-warning">{{ $message }}</div>
-              @enderror
-            </div>
+                        <div class="col-12">
+                            <label class="form-label">Titulo
+                                <small class="text-danger-emphasis opacity-75">(Obrigatório) </small>
+                            </label>
+                            <input type="text" class="form-control" name="titulo"
+                                value="{{ old('titulo') ?? ($post->titulo ?? null) }}">
+                            @error('titulo')
+                                <div class="text-warning">{{ $message }}</div>
+                            @enderror
+                        </div>
 
-            {{-- ckeditor --}}
-            <div class="col-12">
-              <label class="form-label">Conteudo
-                <small class="text-danger-emphasis opacity-75">(Obrigatório) </small>
-              </label>
-              <textarea id="editor" class="ckeditor-classic" name="conteudo">
+                        {{-- ckeditor --}}
+                        <div class="col-12">
+                            <label class="form-label">Conteudo
+                                <small class="text-danger-emphasis opacity-75">(Obrigatório) </small>
+                            </label>
+                            <textarea id="editor" class="ckeditor-classic" name="conteudo">
                 {!! old('conteudo') ?? ($post->conteudo ?? null) !!}
               </textarea>
+                        </div>
+                        {{-- ckeditor --}}
+
+                        <div class="col-12">
+
+                            <label class="form-label">
+                                {{ $post->thumb ? 'Editar Capa' : 'Imagem de Capa' }}
+                            </label>
+                            <input type="file" class="form-control" name="thumb" id="formFile"
+                                accept="image/png, image/jpeg" value="{{ old('thumb') ?? ($post->thumb ?? null) }}">
+
+                            @error('thumb')
+                                <div class="text-warning">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            @unless (empty($post->thumb))
+                                <img src="{{ asset($post->thumb) }}" class="img-thumbnail img-fluid"
+                                    style="max-width: 200px; max-height: 200px;" alt="...">
+                            @endunless
+                        </div>
+
+                        <div class="col-12">
+                            <label class="form-label">Tipo</label>
+                            <select class="form-select" name="tipo" aria-label="Default select example">
+                                <option value="noticia" @selected($post->tipo == 'noticia')>Noticia</option>
+                                <option value="galeria" @selected($post->tipo == 'galeria')>Galeria</option>
+                            </select>
+                        </div>
+
+                        <div class="row mt-4 d-flex align-items-center">
+
+                            <div class="col-7">
+                                <label for="data-publicacao">Selecione uma data para publicação</label>
+                                <input class="form-control" type="date" id="data_publicacao" name="data_publicacao"
+                                    value="{{ old('data_publicacao') ? old('data_publicacao') : ($post->data_publicacao ? \Carbon\Carbon::parse($post->data_publicacao)->format('Y-m-d') : '') }}" />
+                                @error('data_publicacao')
+                                    <div class="text-warning">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="col-5">
+                                <div class="form-check">
+                                    <input class="form-check-input" name="rascunho" type="checkbox" value="1"
+                                        @checked($post->rascunho)>
+                                    <label class="form-check-label" for="flexCheckDefault">Rascunho</label>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-12 mt-3">
+                        <button type="submit"
+                            class="btn btn-primary px-4">{{ $post->slug ? 'Atualizar' : 'Salvar' }}</button>
+                    </div>
+                </form>
             </div>
-            {{-- ckeditor --}}
+        </div>
 
-            <div class="col-12">
-              <label class="form-label">Imagem de capa</label>
-              <input type="file" class="form-control" name="thumb" id="formFile"
-                accept="image/png, image/jpeg"
-                value="{{ old('thumb') ?? ($post->thumb ?? null) }}">
-              @error('thumb')
-                <div class="text-warning">{{ $message }}</div>
-              @enderror
-            </div>
+        @if ($post->id)
+            <x-painel.noticias.form-delete route="post-delete" id="{{ $post->id }}" />
+        @endif
 
-            <div class="col-12">
-              <label class="form-label">Tipo</label>
-              <select class="form-select" name="tipo" aria-label="Default select example">
-                <option value="noticia" @selected($post->tipo == 'noticia')>Noticia</option>
-                <option value="galeria" @selected($post->tipo == 'galeria')>Galeria</option>
-              </select>
-            </div>
-
-            <div class="row mt-4 d-flex align-items-center">
-
-              <div class="col-7">
-                <label for="data-publicacao">Selecione uma data para publicação</label>
-                <input class="form-control" type="date" id="data_publicacao" name="data_publicacao"
-                  value="{{ old('data_publicacao') ? old('data_publicacao') : ($post->data_publicacao ? \Carbon\Carbon::parse($post->data_publicacao)->format('Y-m-d') : '') }}" />
-                @error('data_publicacao')
-                  <div class="text-warning">{{ $message }}</div>
-                @enderror
-              </div>
-
-              <div class="col-5">
-                <div class="form-check">
-                  <input class="form-check-input" name="rascunho" type="checkbox" value="1"
-                    @checked($post->rascunho)>
-                  <label class="form-check-label" for="flexCheckDefault">Rascunho</label>
-                </div>
-              </div>
-
-            </div>
-
-          </div>
-          <div class="col-12 mt-3">
-            <button type="submit"
-              class="btn btn-primary px-4">{{ $post->slug ? 'Atualizar' : 'Salvar' }}</button>
-          </div>
-        </form>
-      </div>
     </div>
-
-    @if ($post->id)
-      <x-painel.noticias.form-delete route="post-delete" id="{{ $post->id }}" />
-    @endif
-
-  </div>
 
 </div>
 <script src="{{ URL::asset('build/libs/@ckeditor/ckeditor5-build-classic/build/ckeditor.js') }}"></script>
 <script>
-  var ckClassicEditor = document.querySelectorAll(".ckeditor-classic")
-  if (ckClassicEditor) {
-    Array.from(ckClassicEditor).forEach(function() {
-      ClassicEditor
-        .create(document.querySelector('.ckeditor-classic'), {
-          ckfinder: {
-            uploadUrl: '{{ route('image-upload') . '?_token=' . csrf_token() }}',
-          }
-        })
-        .then(function(editor) {
-          editor.ui.view.editable.element.style.height = '200px';
-        })
-        .catch(function(error) {
-          console.error(error);
+    var ckClassicEditor = document.querySelectorAll(".ckeditor-classic")
+    if (ckClassicEditor) {
+        Array.from(ckClassicEditor).forEach(function() {
+            ClassicEditor
+                .create(document.querySelector('.ckeditor-classic'), {
+                    ckfinder: {
+                        uploadUrl: '{{ route('image-upload') . '?_token=' . csrf_token() }}',
+                    }
+                })
+                .then(function(editor) {
+                    editor.ui.view.editable.element.style.height = '200px';
+                })
+                .catch(function(error) {
+                    console.error(error);
+                });
         });
-    });
-  }
+    }
 </script>
