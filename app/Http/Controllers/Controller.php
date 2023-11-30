e75a02d044bb5e4850d5537d93184fb11c0439f6
FILIPE PETTER <fgpetter@gmail.com>
2023-11-30 20:05:16 -0300

    cria tabela e funcao para geracao de logs no sistema

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Models\LogsSistema;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * Salva no banco as ações do usuário
+     *
+     * @param array|string $info
+     * @return void
+     */
+    protected function actionLog( $info = null )
+    {
+        $data = [
+            'entidade' => get_class($this),
+            'usuario_nome' => Auth::user()->name ?? 'teste',
+            'acao' => debug_backtrace()[1]['function'],
+            'info' => json_encode($info)
+        ];
+
+        LogsSistema::create($data);
+
+        logger()->info(json_encode($data));
+    }
 }
