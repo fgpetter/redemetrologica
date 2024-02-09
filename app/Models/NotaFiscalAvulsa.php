734038cc6c25ba68421c0f4221b84fa9116101da
FILIPE PETTER <fgpetter@gmail.com>
2024-02-09 14:14:36 -0300

    adocao de migrations e models

---
+++ app/Models/NotaFiscalAvulsa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NotaFiscalAvulsa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'notas_fiscais_avulsas';
+}
