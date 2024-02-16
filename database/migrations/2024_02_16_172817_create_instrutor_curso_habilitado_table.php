09e9704dcab95279a04054264cb469495227ef6a
Matheus Kraus <krausz90@gmail.com>
2024-02-16 17:32:25 -0300

    migration

---
+++ database/migrations/2024_02_16_172817_create_instrutor_curso_habilitado_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('instrutor_curso_habilitado', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('instrutor_id');
+            $table->unsignedBigInteger('curso_id');
+            $table->boolean('habilitado');
+            $table->string('conhecimento');
+            $table->string('experiencia');
+            $table->text('observacoes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('instrutor_curso_habilitado');
+    }
+};
