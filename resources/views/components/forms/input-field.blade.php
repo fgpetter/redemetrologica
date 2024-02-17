d7f3e78b375748873e07517e8ad3e325ef932400
FILIPE PETTER <fgpetter@gmail.com>
2024-02-17 11:22:54 -0300

    ajustes nos templates de formulários

--- resources/views/components/forms/input-field.blade.php
+++ resources/views/components/forms/input-field.blade.php
@@ -15,7 +15,8 @@
 <label class="form-label">{!! $label !!}</label>
 <input {{ $attributes->class(['form-control']) }} type="{{ $type }}" name="{{ $name }}"
     @if ($value) value="{{ $value }}" @endif
-    @if ($required) required @endif @if ($id) id={{ $id }} @endif
+    @if ($required) required @endif 
+    @if ($id) id={{ $id }} @endif
     @if ($placeholder) placeholder="{{ $placeholder }}" @endif
     @if ($uppercase) oninput="this.value = this.value.toUpperCase()" @endif
     @if ($pattern) pattern='{{ $pattern }}' title="{{ $title }}" @endif>
