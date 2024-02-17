c8f98bff15afbb11cf03f5601838b661343d7377
FILIPE PETTER <fgpetter@gmail.com>
2024-02-17 16:00:50 -0300

    refactoring em enderecos

--- resources/views/components/painel/enderecos/modal.blade.php
+++ resources/views/components/painel/enderecos/modal.blade.php
@@ -1,19 +1,19 @@
-<div class="modal fade" id="{{ ($endereco->uid) ? "modal_endereco_$endereco->uid" : "modal_endereco_cadastro"}}" 
-  tabindex="-1" aria-labelledby="{{ ($endereco->uid) ? "modalgridEndereco$endereco->uid" : "modalgridEndereco" }}" aria-modal="true">
+<div class="modal fade" id="{{ (isset($endereco->uid)) ? "modal_endereco_$endereco->uid" : "modal_endereco_cadastro"}}" 
+  tabindex="-1" aria-labelledby="{{ (isset($endereco->uid)) ? "modalgridEndereco$endereco->uid" : "modalgridEndereco" }}" aria-modal="true">
   <div class="modal-dialog modal-dialog-left modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" 
-        id="{{ ($endereco->uid) ? "modalgridEndereco$endereco->uid" : "modalgridEndereco" }}">
+        <h5 class="modal-title" id="{{ (isset($endereco->uid)) ? "modalgridEndereco$endereco->uid" : "modalgridEndereco" }}">
         {{$endereco->endereco ?? 'Cadastrar Endereço'}}
         </h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form action="{{ isset($endereco->uid) ? route('endereco-update', $endereco->uid) : route('endereco-create') }}" method="POST">
+        <form action="{{ (isset($endereco->uid)) ? route('endereco-update', $endereco->uid) : route('endereco-create') }}" method="POST">
           @csrf
-          <div class="row">
-            <x-painel.enderecos.form-endereco :endereco="$endereco ?? ''"/>
+          <div class="row mb-4">
+            <input type="hidden" name="pessoa" value="{{$pessoa->id}}">
+            <x-painel.enderecos.form-endereco :endereco="$endereco ?? null" :pessoa="$pessoa"/>
           </div>
             <div class="col-lg-12">
               <div class="hstack gap-2 justify-content-end">
