08e02cf21c1f3bc3c5ce7875e7a9a075f951be91
Matheus Kraus <krausz90@gmail.com>
2024-03-02 16:44:27 -0300

    FEATURE/sweetalert-67

--- resources/views/components/painel/form/delete.blade.php
+++ resources/views/components/painel/form/delete.blade.php
@@ -1,7 +1,9 @@
-<form class=" form-delete d-flex justify-content-end pb-3" style="margin-top: -2rem" method="POST"
-    action="{{ route($route, $id) }}">
+<form class=" form-delete " method="POST" action="{{ route($route, $id) }}">
     @csrf
-    <button type="submit" class=" botao-delete btn btn-sm btn-danger">Remover curso</button>
+
+    <button class=" botao-delete dropdown-item" type="submit">Deletar</button>
+
+
 </form>
 
 <script>
