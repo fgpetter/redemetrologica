1d47ec34e6e89f2dba90aaa417116e0a16c461c7
FILIPE PETTER <fgpetter@gmail.com>
2024-03-04 16:22:51 -0300

    refatora JS do botão de deletar

--- resources/views/components/painel/cursos/form-delete.blade.php
+++ resources/views/components/painel/cursos/form-delete.blade.php
@@ -1,30 +1,27 @@
-<form class=" form-delete d-flex justify-content-end pb-3" style="margin-top: -2rem" method="POST"
+<form class="form-delete d-flex justify-content-end pb-3" style="margin-top: -2rem" method="POST"
     action="{{ route($route, $id) }}">
     @csrf
-    <button type="submit" class=" botao-delete btn btn-sm btn-danger">Remover curso</button>
+    <button type="submit" onclick="requestConfirmDelete(event)" class="btn btn-sm btn-danger">Remover curso</button>
 </form>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const form = document.querySelector('.form-delete');
-        const deleteButton = form.querySelector('.botao-delete');
+    function requestConfirmDelete(event) {
+        event.preventDefault();
 
-        deleteButton.addEventListener('click', function(e) {
-            e.preventDefault();
-
-            Swal.fire({
-                title: 'Tem certeza?',
-                text: "Você não poderá reverter isso!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sim, excluir!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    form.submit();
-                }
-            });
+        Swal.fire({
+            title: 'Tem certeza?',
+            text: "Você não poderá reverter isso!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, excluir!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const form = document.querySelector('.form-delete');
+                form.submit();
+            }
         });
-    });
+    }
+    
 </script>
--- resources/views/components/painel/form/delete.blade.php
+++ resources/views/components/painel/form/delete.blade.php
@@ -2,35 +2,31 @@
      <form class=" form-delete d-flex justify-content-end pb-3" style="margin-top: -2rem" method="POST"
          action="{{ route($route, $id) }}">
          @csrf
-         <button type="submit" class=" botao-delete btn btn-sm btn-danger">Remover {{ $label }}</button>
+         <button type="button" onclick="requestConfirmDelete(event)" class="btn btn-sm btn-danger">Remover {{ $label }}</button>
      </form>
  @else
      <form class=" form-delete " method="POST" action="{{ route($route, $id) }}">
          @csrf
-         <button class=" botao-delete dropdown-item" type="submit">Deletar</button>
+         <button class="dropdown-item" type="button" onclick="requestConfirmDelete(event)">Deletar</button>
      </form>
  @endif
  <script>
-     document.addEventListener('DOMContentLoaded', function() {
-         const form = document.querySelector('.form-delete');
-         const deleteButton = form.querySelector('.botao-delete');
+    function requestConfirmDelete(event) {
+        event.preventDefault();
 
-         deleteButton.addEventListener('click', function(e) {
-             e.preventDefault();
-
-             Swal.fire({
-                 title: 'Tem certeza?',
-                 text: "Você não poderá reverter isso!",
-                 icon: 'warning',
-                 showCancelButton: true,
-                 confirmButtonColor: '#3085d6',
-                 cancelButtonColor: '#d33',
-                 confirmButtonText: 'Sim, deletar!'
-             }).then((result) => {
-                 if (result.isConfirmed) {
-                     form.submit();
-                 }
-             });
-         });
-     });
+        Swal.fire({
+            title: 'Tem certeza?',
+            text: "Você não poderá reverter isso!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, excluir!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const form = document.querySelector('.form-delete');
+                form.submit();
+            }
+        });
+    }
  </script>
