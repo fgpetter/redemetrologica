235300139e2f7c437da816c59b6f19d5c86f9c5b
FILIPE PETTER <fgpetter@gmail.com>
2024-03-12 19:21:39 -0300

    FIX ajusta erro ao deletar itens com swal

--- resources/views/components/painel/form-delete/delete.blade.php
+++ resources/views/components/painel/form-delete/delete.blade.php
@@ -1,32 +1,34 @@
- @if (isset($label))
-     <form class=" form-delete d-flex justify-content-end pb-3" style="margin-top: -2rem" method="POST"
-         action="{{ route($route, $id) }}">
-         @csrf
-         <button type="button" onclick="requestConfirmDelete(event)" class="btn btn-sm btn-danger">Remover {{ $label }}</button>
-     </form>
- @else
-     <form class=" form-delete " method="POST" action="{{ route($route, $id) }}">
-         @csrf
-         <button class="dropdown-item" type="button" onclick="requestConfirmDelete(event)">Deletar</button>
-     </form>
- @endif
- <script>
-    function requestConfirmDelete(event) {
-        event.preventDefault();
+@if (isset($label))
+<form class=" form-delete d-flex justify-content-end pb-3" style="margin-top: -2rem" method="POST" action="{{ route($route, $id) }}">
+  @csrf
+  <button type="submit"  class="btn btn-sm btn-danger botao-delete">Remover {{ $label }}</button>
+</form>
+@else
+<form class=" form-delete " method="POST" action="{{ route($route, $id) }}">
+  @csrf
+  <button class="dropdown-item botao-delete" type="submit" ">Deletar</button>
+</form>
+@endif
 
-        Swal.fire({
-            title: 'Tem certeza?',
-            text: "Você não poderá reverter isso!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sim, excluir!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                const form = document.querySelector('.form-delete');
-                form.submit();
-            }
-        });
-    }
- </script>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.form-delete');
+    const deleteButton = form.querySelector('.botao-delete');
+    deleteButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      Swal.fire({
+        title: 'Tem certeza?',
+        text: "Você não poderá reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, excluir!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          form.submit();
+        }
+      });
+    });
+  });
+</script>
