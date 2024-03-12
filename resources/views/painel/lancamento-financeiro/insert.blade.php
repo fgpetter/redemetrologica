f0cf2ffadeb2201eb52c80d9e2ca066bc9e04bcd
FILIPE PETTER <fgpetter@gmail.com>
2024-03-12 19:22:16 -0300

    ajusta apresentação da tela de lancamentos financeiros

--- resources/views/painel/lancamento-financeiro/insert.blade.php
+++ resources/views/painel/lancamento-financeiro/insert.blade.php
@@ -6,7 +6,7 @@
 @slot('title') @if ($lancamento->id) Editar Lançamento @else Cadastrar Lançamento @endif @endslot
 @endcomponent
 <div class="row">
-  <div class="col-xl-7 col-xxl-6">
+  <div class="col">
     <x-painel.lancamento-financeiro.insert :lancamento="$lancamento" :pessoas="$pessoas" :centrosdecusto="$centrosdecusto"/>
   </div>
 </div>
