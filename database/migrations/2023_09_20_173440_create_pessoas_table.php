d58e684da9a556eb4a3fbd99ddfcf430bfd079b7
FILIPE PETTER <fgpetter@gmail.com>
2024-04-04 18:15:37 -0300

    ajustes nas tabelas de pessoas e usuários

--- database/migrations/2023_09_20_173440_create_pessoas_table.php
+++ database/migrations/2023_09_20_173440_create_pessoas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('pessoas', function (Blueprint $table) {
             $table->id();
             $table->string('uid');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('nome_razao');
             $table->string('nome_fantasia')->nullable();
             $table->string('cpf_cnpj');
