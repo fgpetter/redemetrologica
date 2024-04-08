014d8a2c37d856b056e3e42aeef0243ea31fc110
FILIPE PETTER <fgpetter@gmail.com>
2024-04-08 10:25:46 -0300

    ajusta input para apresentar asterisco sempre que o campo for required

--- resources/views/components/forms/input-field.blade.php
+++ resources/views/components/forms/input-field.blade.php
@@ -12,7 +12,7 @@
     'title' => null,
 ])
 
-<label class="form-label">{!! $label !!}</label>
+<label class="form-label">{!! $label !!} {!! ($required ? '<span class="text-danger-emphasis">*</span>' : '') !!}</label>
 <input {{ $attributes->class(['form-control']) }} type="{{ $type }}" name="{{ $name }}"
     @if ($value) value="{{ $value }}" @endif
     @if ($required) required @endif 
