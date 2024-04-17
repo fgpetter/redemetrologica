17e9fbe3ae5ee0649d142c89a187f826fe942aff
FILIPE PETTER <fgpetter@gmail.com>
2024-04-17 15:10:55 -0300

    adiciona icone de informacoes no input

--- resources/views/components/forms/input-field.blade.php
+++ resources/views/components/forms/input-field.blade.php
@@ -9,9 +9,14 @@
     'uppercase' => false,
     'pattern' => null,
     'title' => null,
+    'tooltip' => null
 ])
 
-<label class="form-label">{!! $label !!} {!! ($required ? '<span class="text-danger-emphasis">*</span>' : '') !!}</label>
+<label class="form-label">{!! $label !!} {!! ($required ? '<span class="text-danger-emphasis"> * </span>' : '') !!}</label>
+@if ($tooltip)
+  <span data-bs-toggle="tooltip" data-bs-html="true" title="{{ $tooltip }}">
+  <i class="ri-information-line align-middle text-warning-emphasis" style="font-size: 1rem"></i></span>
+@endif
 <input {{ $attributes->class(['form-control']) }} type="{{ $type }}" name="{{ $name }}"
     @if ($value) value="{{ $value }}" @endif
     @if ($required) required @endif 
