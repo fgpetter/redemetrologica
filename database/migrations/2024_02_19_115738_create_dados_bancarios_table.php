4021447ec10e2769dee6899179b75e302cdc90ca
Filipe Petter <fgpetter@gmail.com>
2024-12-03 16:07:04 -0300

    FIX - adiciona migration de dados bancários

---
+++ database/migrations/2024_02_19_115738_create_dados_bancarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Query\Expression;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dados_bancarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid')->default(new Expression("(replace(left(uuid(),12),_utf8mb3'-',_utf8mb4'0'))"))->unique();
+            $table->foreignId('pessoa_id')->constrained();
+            $table->string('nome_conta')->nullable();
+            $table->string('nome_banco');
+            $table->string('cod_banco')->nullable();
+            $table->string('agencia');
+            $table->string('conta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dados_bancarios');
+    }
+};
