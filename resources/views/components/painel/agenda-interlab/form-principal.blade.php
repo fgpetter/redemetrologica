8c98dda7011bbfc5140b0944b5596929783c8b2e
Filipe Petter <fgpetter@gmail.com>
2024-12-14 11:03:09 -0300

    ajustes visuais para mobile e tablets

--- resources/views/components/painel/agenda-interlab/form-principal.blade.php
+++ resources/views/components/painel/agenda-interlab/form-principal.blade.php
@@ -14,7 +14,7 @@
   </div>
 
   <div class="row mt-3">
-    <div class="col-sm-3">
+    <div class="col-lg-3">
       <x-forms.input-select name="status" label="Status">
         <option @selected($agendainterlab->status == 'AGENDADO') value="AGENDADO">AGENDADO</option>
         <option @selected($agendainterlab->status == 'CONFIRMADO') value="CONFIRMADO">CONFIRMADO</option>
@@ -23,19 +23,19 @@
       @error('status') <div class="text-warning">{{ $message }}</div> @enderror
     </div>
 
-    <div class="col-sm-2">
+    <div class="col-6 col-lg-4 col-xl-2">
       <x-forms.input-field :value="old('data_inicio') ?? ($agendainterlab->data_inicio?->format('Y-m-d') ?? null)" type="date" name="data_inicio"
         label="Data Inicio"/>
       @error('data_inicio') <div class="text-warning">{{ $message }}</div> @enderror
     </div>
 
-    <div class="col-sm-2">
+    <div class="col-6 col-lg-4 col-xl-2">
       <x-forms.input-field :value="old('data_fim') ?? ($agendainterlab->data_fim?->format('Y-m-d') ?? null)" type="date" name="data_fim"
         label="Data Final"/>
       @error('data_fim') <div class="text-warning">{{ $message }}</div> @enderror
     </div>
 
-    <div class="col-sm-3">
+    <div class="col-6 col-lg-3">
       <x-forms.input-select name="certificado" label="Emitir certificado por:">
         <option @selected($agendainterlab->certificado == 'EMPRESA') value="EMPRESA">EMPRESA</option>
         <option @selected($agendainterlab->certificado == 'PARTICIPANTE') value="PARTICIPANTE">PARTICIPANTE</option>
@@ -46,7 +46,7 @@
   </div>
 
   <div class="row mt-3">
-    <div class="col-sm-3">
+    <div class="col-6 col-lg-3">
       <div class="form-check bg-light rounded mt-4 check-bg" style="padding: 0.8rem 1.8rem 0.8rem;">
         <input class="form-check-input" name="inscricao" value="1" id="inscricao" type="checkbox"
           @checked($agendainterlab->inscricao ?? false)>
@@ -55,7 +55,7 @@
       @error('inscricao') <div class="text-warning">{{ $message }}</div> @enderror
     </div>
 
-    <div class="col-sm-3">
+    <div class="col-6 col-lg-3">
       <div class="form-check bg-light rounded mt-4 check-bg" style="padding: 0.8rem 1.8rem 0.8rem;">
         <input class="form-check-input" name="site" value="1" id="site" type="checkbox"
           @checked($agendainterlab->site ?? false)>
@@ -64,7 +64,7 @@
       @error('site') <div class="text-warning">{{ $message }}</div> @enderror
     </div>
 
-    <div class="col-sm-3">
+    <div class="col-6 col-lg-3">
       <div class="form-check bg-light rounded mt-4 check-bg" style="padding: 0.8rem 1.8rem 0.8rem;">
         <input class="form-check-input" name="destaque" value="1" id="destaque" type="checkbox"
           @checked($agendainterlab->destaque ?? false)>
@@ -73,31 +73,31 @@
       @error('destaque') <div class="text-warning">{{ $message }}</div> @enderror
     </div>
 
-    <div class="col-12 col-sm-9 mt-4">
+    <div class="col-12 mt-4">
       <div class="card border rouded shadow-none">
         <div class="card-body">
           <h6 class="card-subtitle mb-2">Valores de inscrição por rodada conforme região</h6>
           <div class="row">
 
-            <div class="col col-sm-3">
+            <div class="col-12 col-lg-3">
               <x-forms.input-field :value="old('valor_rs') ?? ($agendainterlab->valor_rs ?? null)" type="text" name="valor_rs"
                 label="Valor base - RS" class="money"/>
               @error('valor_rs') <div class="text-warning">{{ $message }}</div> @enderror
             </div>
 
-            <div class="col col-sm-3">
+            <div class="col-12 col-lg-3">
               <x-forms.input-field :value="old('valor_s_se') ?? ($agendainterlab->valor_s_se ?? null)" type="text" name="valor_s_se"
                 label="Sul e Sudeste" class="money"/>
               @error('valor_s_se') <div class="text-warning">{{ $message }}</div> @enderror
             </div>
 
-            <div class="col col-sm-3">
+            <div class="col-12 col-lg-3">
               <x-forms.input-field :value="old('valor_co') ?? ($agendainterlab->valor_co ?? null)" type="text" name="valor_co"
                 label="Centro Oeste" class="money"/>
               @error('valor_co') <div class="text-warning">{{ $message }}</div> @enderror
             </div>
 
-            <div class="col col-sm-3">
+            <div class="col-12 col-lg-3">
               <x-forms.input-field :value="old('valor_n_ne') ?? ($agendainterlab->valor_n_ne ?? null)" type="text" name="valor_n_ne"
                 label="Norte e Nordeste" class="money"/>
               @error('valor_n_ne') <div class="text-warning">{{ $message }}</div> @enderror
