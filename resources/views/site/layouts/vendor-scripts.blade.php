d9607db0262ef06ed31bd01353bf17c00a0714fc
Filipe Petter <fgpetter@gmail.com>
2025-01-04 14:27:41 -0300

    FIX - corrige erro de duplicacao de js carregado

--- resources/views/site/layouts/vendor-scripts.blade.php
+++ resources/views/site/layouts/vendor-scripts.blade.php
@@ -2,7 +2,6 @@
 <script src="{{ URL::asset('build/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ URL::asset('build/libs/sweetalert2/sweetalert2.all.min.js') }}"></script>
 <script src="{{ URL::asset('build/libs/simplebar/simplebar.min.js') }}"></script>
-<script src="{{ URL::asset('build/js/custom.js') }}"></script>
 <script src="{{ Vite::asset('resources/js/custom.js') }}"></script>
 
 @yield('script')
