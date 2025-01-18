bb80359b9df22eb53227b6bcfe9cd357b58bf3aa
Filipe Petter <fgpetter@gmail.com>
2025-01-18 14:46:23 -0300

    FIX - corrige bug do framework ao deslogar com sessao expirada

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -11,7 +12,17 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [
-        //
-    ];
-}
+    protected $except = [ '/logout' ];
+
+    public function handle($request, Closure $next)
+    {
+        if($request->route()->named('logout')) {
+            if (auth()->check()) {
+                auth()->logout();
+            }
+
+            return redirect('/');
+        }
+
+        return parent::handle($request, $next);
+    }}
