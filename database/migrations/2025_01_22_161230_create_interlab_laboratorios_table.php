a2ae95cbd2a1dfa3f88ee2c7b729756d8a7848da
Filipe Petter <fgpetter@gmail.com>
2025-01-30 17:15:55 -0300

    fix: ajusta nome de arquivo de migracao

--- database/migrations/2025_01_22_161230_create_interlab_laboratorios_table.php
+++ database/migrations/2025_01_22_161230_create_interlab_laboratorios_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('interlab_inscritos');
+        Schema::dropIfExists('interlab_laboratorios');
     }
 };
