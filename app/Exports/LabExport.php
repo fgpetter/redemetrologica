736e8ba49cc2cf507a3225f4796a4958183fe355
Filipe Petter <fgpetter@gmail.com>
2025-02-13 15:09:20 -0300

    fix: ajusta erro na classe LabExport

--- app/Exports/LabExport.php
+++ app/Exports/LabExport.php
@@ -10,12 +10,12 @@ use Maatwebsite\Excel\Concerns\FromView;
 class LabExport implements FromView
 {
 
-    public function __construct(AgendaInterlab $agendainterlab) {}
+    public function __construct(public AgendaInterlab $agendainterlab) {}
 
     public function view(): View
     {
         return view('excel.enderecos-laboratorios',[
-            'inscritos' => InterlabInscrito::where('agenda_interlab_id', 3)
+            'inscritos' => InterlabInscrito::where('agenda_interlab_id', $this->agendainterlab->id)
               ->with(['pessoa', 'empresa', 'laboratorio.endereco'])
               ->get()
           ]);
