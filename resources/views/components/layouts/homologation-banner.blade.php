d9f59eedf01b3301011fb3c1c8d3363d8848dabd
Matheus Kraus <krausz90@gmail.com>
2025-07-03 08:05:24 -0300

    diminuir a barra

--- resources/views/components/layouts/homologation-banner.blade.php
+++ resources/views/components/layouts/homologation-banner.blade.php
@@ -1,5 +1,5 @@
 @if(env('APP_HOMOLOG'))
-    <div style="background-color: yellow; color: black; text-align: center; padding: 3px; font-weight: bold;">
+    <div style="background-color: yellow; color: black; text-align: center;  font-weight: bold;">
         Ambiente de Homologação
     </div>
 @endif
