b24668c20738e8e370020a6ec793ce5d21b9a5e2
Matheus Kraus <krausz90@gmail.com>
2025-08-01 08:45:09 -0300

    Carta Reconhecimento - WIP

--- app/Actions/GenerateDocxFromTemplateAction.php
+++ app/Actions/GenerateDocxFromTemplateAction.php
@@ -8,7 +8,7 @@ use PhpOffice\PhpWord\TemplateProcessor;
 class GenerateDocxFromTemplateAction
 {
     
-    public function execute(string $templatePath, array $data, string $outputRelativePath): string
+    public function execute(string $templatePath, array $data, array  $blocks, string $outputRelativePath): string
     {
         if (!file_exists($templatePath)) {
             throw new \Exception("O template do documento não foi encontrado em {$templatePath}");
@@ -17,6 +17,16 @@ class GenerateDocxFromTemplateAction
         $templateProcessor = new TemplateProcessor($templatePath);
 
         $templateProcessor->setValues($data);
+
+        foreach ($blocks as $blockName => $replacements) {
+            $templateProcessor->cloneBlock(
+                $blockName,
+                0,      // ignora o número fixo; usa size($replacements)
+                true,   // remove as tags ${BLOCK}/${/BLOCK}
+                false,  // sem quebras extras
+                $replacements
+            );
+        }
 
         $absolutePathToSave = Storage::disk('public')->path($outputRelativePath);
 
--- app/Livewire/Avaliacoes/AgendaAvaliacoesCartas.php
+++ app/Livewire/Avaliacoes/AgendaAvaliacoesCartas.php
@@ -2,39 +2,62 @@
 
 namespace App\Livewire\Avaliacoes;
 
-use App\Actions\GenerateDocxFromTemplateAction;
-use App\Models\AgendaAvaliacao;
 use Livewire\Component;
+use App\Models\AgendaAvaliacao;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use App\Actions\GenerateDocxFromTemplateAction;
 
 class AgendaAvaliacoesCartas extends Component
 {
     public AgendaAvaliacao $avaliacao;
 
-    public function mount(AgendaAvaliacao $avaliacao)
-    {
-        $this->avaliacao = $avaliacao->load('areas');
-    }
+
 
 
     public function gerarCartaReconhecimento()
     {
+        $this->avaliacao->load('areas.avaliador.pessoa' ,'laboratorio.laboratoriosInternos');
         $data = [
-            'laboratorio' => $this->avaliacao->id ?? 'Não informado',
-            'data' => now()->format('d/m/Y'),
-            'outro_parametro' => 'Outro parâmetro',
-            'outro_parametro2' => 'Outro parâmetro',
-            'outro_parametro3' => 'Outro parâmetro',
-            'outro_parametro4' => 'Outro parâmetro'
+            'data_atual' => now()->format('d/m/Y'),
+            'nome_laboratorio' => $this->avaliacao->laboratorio->nome_laboratorio ?? 'Não informado',
+            'data_fim' => $this->avaliacao->data_fim ? \Carbon\Carbon::parse($this->avaliacao->data_fim)->format('d/m/Y') : 'Não informado',
+            'data_retorno' => $this->avaliacao->retorno_fr06 ? \Carbon\Carbon::parse($this->avaliacao->retorno_fr06)->format('d/m/Y') : 'Não informado',
+            'data_evidencias' => $this->avaliacao->data_acoes_corretivas ? \Carbon\Carbon::parse($this->avaliacao->data_acoes_corretivas)->format('d/m/Y') : 'Não informado',
         ];
 
-        // define onde está o template 
-        $templatePath = storage_path('app/templates/Carta_de_Reconhecimento.docx');
-        $outputRelativePath = 'docs/Carta_de_Reconhecimento_' . now()->timestamp . '.docx';
+        $areas = $this->avaliacao->areas->map(function($area) {
+            return [
+                'area_nome' => $area->areaAtuacao->descricao ?? 'Não informado',
+            ];
+        })->toArray();
 
-        // Executa a ação
+        $avaliadores = $this->avaliacao->areas->map(function ($area) {
+            return [
+                'avaliador_nome' => $area->avaliador->pessoa->nome_razao ?? 'Não informado',
+                'avaliador_email' => $area->avaliador->pessoa->email ?? 'Não informado',
+            ];
+        })->toArray();
+
+        //garante a repetição de linhas no template
+        $blocks = [
+            'AREAS_BLOCK'  => $areas,
+            'AVAL_BLOCK' => $avaliadores,
+        ];
+
+
+        // define entradas e saidas
+        $labSlug = Str::slug($this->avaliacao->laboratorio->nome_laboratorio ?? 'laboratorio');
+        $templatePath = storage_path('app/templates/Carta_de_Reconhecimento.docx');
+        $outputRelativePath = "docs/Carta_de_Reconhecimento_{$labSlug}_" . now()->timestamp . ".docx";
+
+
+
         try {
             $gerar = (new GenerateDocxFromTemplateAction())
-                ->execute($templatePath, $data, $outputRelativePath);
+                ->execute($templatePath, $data, $blocks, $outputRelativePath);
+
+            return Storage::disk('public')->download($gerar);
         } catch (\Exception $e) {
             $this->addError('template', 'Erro ao gerar documento: ' . $e->getMessage());
             return;
