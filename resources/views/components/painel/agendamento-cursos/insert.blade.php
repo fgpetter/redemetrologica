cbf34bc41dfeed7541ffbb77168669a76b0eb7f7
Matheus Kraus <krausz90@gmail.com>
2025-12-12 21:50:12 -0300

    Adicionar componente livewire para inscrição de alunos

--- resources/views/components/painel/agendamento-cursos/insert.blade.php
+++ resources/views/components/painel/agendamento-cursos/insert.blade.php
@@ -49,10 +49,12 @@
                         <h5 class="h5 mt-3">Inscritos</h5>
                     </div>
                     <div class="col d-flex justify-content-end align-items-center gap-1">
-                        <a href="#" class="btn btn-sm btn-success" data-bs-toggle="modal"
-                            data-bs-target="#adicionaInscritoModal">
-                            <i class="ri-add-line align-bottom"></i> Adicionar inscrito
-                        </a>
+                        @if ($agendacurso?->tipo_agendamento != 'IN-COMPANY')
+                            <a href="#" class="btn btn-sm btn-success" data-bs-toggle="modal"
+                                data-bs-target="#adicionaInscritoModal">
+                                <i class="ri-add-line align-bottom"></i> Adicionar inscrito
+                            </a>
+                        @endif
                         @if ($tipoagenda == 'ABERTO' && $agendacurso?->id && $agendacurso?->inscritos()->count() > 0)
                             <a href="{{ route('agendamento-curso.export-lista-presenca', $agendacurso) }}"
                                 class="btn btn-sm btn-primary">
@@ -60,15 +62,6 @@
                             </a>
                         @endif
 
-                        @if ($agendacurso?->tipo_agendamento == 'IN-COMPANY')
-                            <span data-bs-toggle="tooltip" data-bs-html="true"
-                                title="Somente se já tiver empresa atrelada ao curso In-Company">
-                                <a href="#" class="btn btn-sm btn-primary" data-bs-toggle="modal"
-                                    data-bs-target="#importaInscritosModal">
-                                    <i class="ri-file-excel-line align-bottom"></i> Adicionar por lista
-                                </a>
-                            </span>
-                        @endif
 
 
                     </div>
@@ -79,26 +72,7 @@
 
                 <x-painel.agendamento-cursos.modal-insere-participante :agendacurso="$agendacurso" :empresas="$empresas"
                     :pessoas="$pessoas" />
-                {{-- Modal: importaInscritosModal --}}
-                @if ($agendacurso?->tipo_agendamento == 'IN-COMPANY' && $agendacurso?->empresa_id)
-                    <div wire:ignore.self class="modal fade" id="importaInscritosModal" tabindex="-1"
-                        aria-labelledby="importaInscritosModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-lg">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">Importar inscritos</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Fechar"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <livewire:cursos.cursoinscrito-import :agendacurso="$agendacurso" :key="$agendacurso->id" />
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endif
-                {{-- Modal: importaInscritosModal --}}
+                
             </div>
 
             <div class="tab-pane" id="despesas" role="tabpanel"> <!-- despesas -->
