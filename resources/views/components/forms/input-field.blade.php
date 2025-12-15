f2287b70a255105545dd659a204372925d7d47b7
Filipe Petter <fgpetter@gmail.com>
2025-12-15 12:09:38 -0300

    FIX: adiciona alerta de erro mais claro em campo obrigatorio

--- resources/views/components/forms/input-field.blade.php
+++ resources/views/components/forms/input-field.blade.php
@@ -15,18 +15,24 @@
     'list' => null,
 ])
 
-<label class="form-label mb-0">{!! $label !!} {!! ($required ? '<span class="text-danger-emphasis"> * </span>' : '') !!}</label>
-@if ($tooltip)
-  <span data-bs-toggle="tooltip" data-bs-html="true" title="{{ $tooltip }}">
-  <i class="ri-information-line align-middle text-warning-emphasis" style="font-size: 1rem"></i></span>
-@endif
-<input {{ $attributes->class(['form-control']) }} type="{{ $type }}" name="{{ $name }}"
-    @if ($value) value="{{ $value }}" @endif
-    @if ($required) required @endif 
-    @if ($id) id={{ $id }} @endif
-    @if ($placeholder) placeholder="{{ $placeholder }}" @endif
-    @if ($uppercase) oninput="this.value = this.value.toUpperCase()" @endif
-    @if ($pattern) pattern='{{ $pattern }}' title="{{ $title }}" @endif
-    @if ($list) list={{ $list }} @endif
-    @readonly($readonly) 
-    @disabled($disabled) >
+<div x-data="{ showError: false }">
+    <label class="form-label mb-0">{!! $label !!} {!! ($required ? '<span class="text-danger-emphasis"> * </span>' : '') !!}</label>
+    @if ($tooltip)
+      <span data-bs-toggle="tooltip" data-bs-html="true" title="{{ $tooltip }}">
+      <i class="ri-information-line align-middle text-warning-emphasis" style="font-size: 1rem"></i></span>
+    @endif
+    <input {{ $attributes->class(['form-control']) }} type="{{ $type }}" name="{{ $name }}"
+        x-on:invalid="showError = true; $el.focus()"
+        x-on:input="showError = false"
+        :class="{ 'is-invalid': showError }"
+        @if ($value) value="{{ $value }}" @endif
+        @if ($required) required @endif 
+        @if ($id) id={{ $id }} @endif
+        @if ($placeholder) placeholder="{{ $placeholder }}" @endif
+        @if ($uppercase) oninput="this.value = this.value.toUpperCase()" @endif
+        @if ($pattern) pattern='{{ $pattern }}' title="{{ $title }}" @endif
+        @if ($list) list={{ $list }} @endif
+        @readonly($readonly) 
+        @disabled($disabled) >
+    <span x-show="showError" x-cloak class="text-danger small">Obrigatório</span>
+</div>
--- resources/views/components/forms/input-textarea.blade.php
+++ resources/views/components/forms/input-textarea.blade.php
@@ -9,15 +9,21 @@
   'sublabel' => null,
 ])
 
-<label class="form-label mb-0">{!! $label !!} {!! ($required ? '<span class="text-danger-emphasis"> * </span>' : '') !!}</label>
-@if ($tooltip)
-  <span data-bs-toggle="tooltip" data-bs-html="true" title="{{ $tooltip }}">
-  <i class="ri-information-line align-middle text-warning-emphasis" style="font-size: 1rem"></i></span>
-@endif
-@if($sublabel) <br> <small class="form-text text-muted">{{ $sublabel }}</small> @endif
+<div x-data="{ showError: false }">
+  <label class="form-label mb-0">{!! $label !!} {!! ($required ? '<span class="text-danger-emphasis"> * </span>' : '') !!}</label>
+  @if ($tooltip)
+    <span data-bs-toggle="tooltip" data-bs-html="true" title="{{ $tooltip }}">
+    <i class="ri-information-line align-middle text-warning-emphasis" style="font-size: 1rem"></i></span>
+  @endif
+  @if($sublabel) <br> <small class="form-text text-muted">{{ $sublabel }}</small> @endif
 
-<textarea {{ $attributes->class(['form-control']) }} name={{ $name }} rows="3"
-@if ($id) id={{ $id }} @endif
-@if ($required) required @endif
-@if ($uppercase) oninput="this.value = this.value.toUpperCase()" @endif>{{ $slot }}</textarea>
-@if ($helper) <small class="form-text text-muted">{{ $helper }}</small> @endif
+  <textarea {{ $attributes->class(['form-control']) }} name={{ $name }} rows="3"
+    x-on:invalid="showError = true; $el.focus()"
+    x-on:input="showError = false"
+    :class="{ 'is-invalid': showError }"
+    @if ($id) id={{ $id }} @endif
+    @if ($required) required @endif
+    @if ($uppercase) oninput="this.value = this.value.toUpperCase()" @endif>{{ $slot }}</textarea>
+  <span x-show="showError" x-cloak class="text-danger small">Obrigatório</span>
+  @if ($helper) <small class="form-text text-muted">{{ $helper }}</small> @endif
+</div>
