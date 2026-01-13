c7afd946f851c24673c7bb690024f267956bb1f5
Matheus Kraus <krausz90@gmail.com>
2026-01-13 14:02:24 -0300

    ajusta index.blade antigo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,80 +1,76 @@
 @extends('layouts.master')
 
-@section('title')
-    Painel
-@endsection
+@section('title') Painel @endsection
 
 @section('content')
-    @component('components.breadcrumb')
-        @slot('li_1')
-            Inicio
-        @endslot
-        @slot('title')
-            Painel
-        @endslot
-    @endcomponent
+  @component('components.breadcrumb')
+    @slot('li_1') Inicio @endslot
+    @slot('title') Painel @endslot
+  @endcomponent
 
-    @if (auth()->user()->pessoa)
+  @if( auth()->user()->pessoa )
 
-        {{-- Habilita impersonamento --}}
-        @canany(['admin', 'funcionario'])
-            <div class="col-6">
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <strong>Selecione um usuário para assumir a visão de cliente:</strong>
-                        <form action="{{ route('impersonate') }}" method="POST">
-                            @csrf
-                            <div class="row">
-                                <div class="col">
-                                    <select class="form-control" data-choices name="user_id" id="user_id">
-                                        <option value="">Selecione um usuário</option>
-                                        @foreach (App\Models\User::whereHas('permissions', fn($q) => $q->where('permission', 'cliente'))->select('id', 'name', 'email')->orderBy('name')->get() as $user)
-                                            <option value="{{ $user->id }}">
-                                                {{ $user->name . ' - ' . $user->email }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="col-2">
-                                    <button type="submit" class="btn btn-primary">
-                                        Personificar
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+    {{-- Habilita impersonamento --}}
+    @canany(['admin','funcionario'])
+      <div class="col-6">
+        <div class="card mb-4">
+          <div class="card-body">
+            <strong>Selecione um usuário para assumir a visão de cliente:</strong>
+            <form action="{{ route('impersonate') }}" method="POST">
+              @csrf
+              <div class="row">
+                <div class="col-9">
+                  <select class="form-control" data-choices name="user_id" id="user_id">
+                    <option value="">Selecione um usuário</option>
+                    @foreach(App\Models\User::whereHas('permissions', fn($q) => $q->where('permission', 'cliente'))
+                      ->select('id', 'name', 'email')->orderBy('name')->get() as $user)
+                      <option value="{{ $user->id }}">
+                        {{ $user->name .' - '. $user->email }}
+                      </option>
+                    @endforeach
+                  </select>
                 </div>
-            </div>
-        @endcanany
+                <div class="col-3">
+                  <button type="submit" class="btn btn-primary">
+                    Personificar
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    @endcanany
 
-        @if (session('impersonator_id'))
-            <div class="alert alert-warning">
-                <form action="{{ route('impersonate-stop') }}" method="POST" class="d-inline">
-                    @csrf
-                    <span class="text-secondary"> Atuando como: {{ auth()->user()->name }} </span>
-                    <button type="submit" class="btn btn-warning btn-sm ms-4">
-                        Parar Personificação
-                    </button>
-                </form>
-            </div>
-        @endif
+    @if(session('impersonator_id'))
+      <div class="alert alert-warning">
+        <form action="{{ route('impersonate-stop') }}" method="POST" class="d-inline">
+          @csrf
+          <span class="text-secondary"> Atuando como: {{ auth()->user()->name }} </span>
+          <button type="submit" class="btn btn-warning btn-sm ms-4">
+            Parar Personificação
+          </button>
+        </form>
+      </div>
+    @endif
 
 
-        @if (auth()->user()->pessoa->funcionario)
-            {{-- carega componentes referentes ao funcionário e área --}}
-            Painel de funcionários
-        @else
-            @if (session('curso'))
-                {{-- carrega componente em app\View\Components\Painel\PainelCliente\ConfirmaInscricao --}}
-                {{-- <x-painel.painel-cliente.confirma-inscricao /> --}}
-                <livewire:painel-cliente.confirm-inscricao-curso />
-            @endif
+    @if(auth()->user()->pessoa->funcionario)
+      {{-- carega componentes referentes ao funcionário e área --}}
+      Painel de funcionários
+    @else
 
-            @if (session('interlab'))
-                {{-- carrega componente em app\View\Components\Painel\PainelCliente\ConfirmaInscricao --}}
-                <x-painel.painel-cliente.confirma-inscricao-interlab />
-            @endif
-            {{-- todo - melhorar lista de cursos inscritos e convites --}}
+      @if (session('curso'))
+        <livewire:painel-cliente.confirm-inscricao-curso />
+      @endif
+
+      @if ( session('interlab') )
+         @include('painel.painel-cliente.nova-inscricao-pd')
+      @elseif( auth()->user()->pessoa->interlabs()->count() > 0 )
+        <x-painel.painel-cliente.inscritos-interlab :interlabs="auth()->user()->pessoa->interlabs()->get()" />
+      @endif
+
+      {{-- todo - melhorar lista de cursos inscritos e convites --}}
             @if (auth()->user()->pessoa->cursos->isNotEmpty())
                 <div class="col-12 col-xxl-6 col-xl-8">
                     <div class="card">
@@ -215,19 +211,6 @@
 
         @endif
 
-    @endif
+  @endif
 
-@endsection
-
-@section('script')
-    <script defer>
-        const element = document.getElementById('user_id')
-        if (element) {
-            const choices = new Choices(element, {
-                searchFields: ['label'],
-                maxItemCount: -1,
-                allowHTML: true
-            });
-        }
-    </script>
 @endsection
