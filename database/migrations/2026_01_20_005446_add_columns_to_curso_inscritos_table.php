b69c07baa4bd9ad5ae52d28f45e544094ba9a80f
Matheus Kraus <krausz90@gmail.com>
2026-01-20 03:53:30 -0300

    migration para adicionar colunas em curso_inscritos e remover foreignKey e permitir o delete.

---
+++ database/migrations/2026_01_20_005446_add_columns_to_curso_inscritos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('curso_inscritos', function (Blueprint $table) {
+            $table->string('nome')->after('agenda_curso_id');
+            $table->string('email')->after('nome');
+            $table->string('telefone')->nullable()->after('email');
+            $table->unsignedBigInteger('lancamento_financeiro_id')->nullable()->after('telefone');
+            $table->dropForeign('curso_inscritos_pessoa_id_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('curso_inscritos', function (Blueprint $table) {
+            $table->dropColumn(['nome', 'email', 'telefone', 'lancamento_financeiro_id']);
+            $table->foreign('pessoa_id')->references('id')->on('pessoas');
+        });
+    }
+};
