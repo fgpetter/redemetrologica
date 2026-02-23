fbd7247a9a4331e5de48786d301d6c7b149fc7f0
Filipe Petter <fgpetter@gmail.com>
2026-02-23 18:01:14 -0300

    FIX: ajusta PDF de certificado

--- resources/views/certificados/certificado-interlab.blade.php
+++ resources/views/certificados/certificado-interlab.blade.php
@@ -29,7 +29,7 @@
 
          .footer {
             position: fixed;
-            bottom: 0mm;   
+            bottom: 0mm;
             left: 0;
             width: 100%;
         }
@@ -43,13 +43,15 @@
 
 <body class="text-center bg-white">
     <div class="certificate-container p-2">
+        <div style="position: fixed; left: 0; top: 50%; transform: translateY(-50%); writing-mode: vertical-lr; z-index: 999;">
+            FR74 - rev03
+        </div>
         <div class="text-center mb-3">
             <img src="{{ resource_path('images/certificados/LOGO_REDE_COLOR.png') }}" alt="Logo" style="width: 300px;">
         </div>
 
         <div class="mt-2">
             <h1 class="display-4 font-weight-bold">CERTIFICADO</h1>
-            <h2 class="h2 font-weight-bold my-3">Participação</h2>
 
             <p class="h3">Associação Rede de Metrologia e Ensaios do RS certifica que</p>
 
@@ -57,7 +59,7 @@
 
             <p class="h3">Participou do</p>
 
-            <p class="h4 font-weight-bold mt-3 mb-3">{{ $participante->agendaInterlab->interlab->nome }}</p>
+            <p class="h4 font-weight-bold mt-3 mb-3">{{ $participante->agendaInterlab->interlab->nome }} - {{ $participante->agendaInterlab->ano_referencia }}</p>
             @php
                 setlocale(LC_TIME, 'pt_BR.UTF-8');
                 $data = \Carbon\Carbon::now();
@@ -65,22 +67,21 @@
                     'Porto Alegre, ' .
                     $data->format('d') .
                     ' de ' .
-                    ucfirst(strftime('%B', $data->getTimestamp())) .
+                    ucfirst($data->locale('pt_BR')->getTranslatedMonthName()) .
                     ' de ' .
                     $data->format('Y');
             @endphp
 
             <p class="h6 mt-5">{{ $dataFormatada }}</p>
 
-            <div class="mt-3">
-                <img src="{{ resource_path('images/certificados/assinatura.jpg') }}" alt="Assinatura">
+            <div class="mt-4">
+                <img src="{{ resource_path('images/certificados/assinatura_joao_redemetrologica.png') }}" style="height: 140px;">
             </div>
         </div>
         
     </div>
     <div class="footer">
-        <img src="{{ resource_path('images/certificados/assets_antigos/rodape_atualizado.png') }}"
-             alt="Rodapé">
+        <img src="{{ resource_path('images/certificados/rodape_certificado_pep_redemetrologica.png') }}">
     </div>
 </body>
 
