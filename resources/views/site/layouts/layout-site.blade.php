39c2aaf05892f398c5e930869269f42de49b1813
Filipe Petter <fgpetter@gmail.com>
2026-03-04 18:59:04 -0300

    fix: impede interrupcao do carregamento por script

--- resources/views/site/layouts/layout-site.blade.php
+++ resources/views/site/layouts/layout-site.blade.php
@@ -9,8 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta name="adopt-website-id" content="508299ca-d188-4c27-a881-d9f42be72dbd" />
-    <script src="//tag.goadopt.io/injector.js?website_code=508299ca-d188-4c27-a881-d9f42be72dbd" 
-    class="adopt-injector"></script>
+    <script src="//tag.goadopt.io/injector.js?website_code=508299ca-d188-4c27-a881-d9f42be72dbd" class="adopt-injector" defer ></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
