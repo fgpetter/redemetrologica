0eed37b6245224e65d9a7d7905b575ea40c46074
Filipe Petter <fgpetter@gmail.com>
2026-04-26 20:36:10 -0300

    REFAC: update AreaAtuacaoController and add new error pages
    - Refactored AreaAtuacaoController to improve method organization and code readability.
    - Added new views for 404, 500, 503, and offline error pages to enhance user experience.
    - Updated existing view references for consistency in naming conventions.

--- app/Http/Controllers/AreaAtuacaoController.php
+++ app/Http/Controllers/AreaAtuacaoController.php
@@ -6,91 +6,86 @@ use App\Models\AreaAtuacao;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-
 class AreaAtuacaoController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   */
-  public function index(Request $request): View
-{
-    $sortDirection = $request->input('order', 'asc');
-    $sortField     = $request->input('orderBy', 'descricao');
-    $searchTerm    = $request->input('buscadecricao');
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): View
+    {
+        $sortDirection = $request->input('order', 'asc');
+        $sortField = $request->input('orderBy', 'descricao');
+        $searchTerm = $request->input('buscadecricao');
 
-    $areas_atuacao = AreaAtuacao::when($searchTerm, function ($query) use ($searchTerm) {
+        $areas_atuacao = AreaAtuacao::when($searchTerm, function ($query) use ($searchTerm) {
             $query->where('descricao', 'LIKE', "%{$searchTerm}%");
         })
-        ->orderBy($sortField, $sortDirection)
-        ->paginate(15)
-        ->withQueryString();
+            ->orderBy($sortField, $sortDirection)
+            ->paginate(15)
+            ->withQueryString();
 
-    return view('painel.area-atuacao.index', ['areas_atuacao' => $areas_atuacao]);
-}
-
-
-
-  /**
-   * Store a newly created resource in storage.
-   */
-  public function store(Request $request)
-  {
-    $validated = $request->validate(
-      [
-        'descricao' => ['required', 'string'],
-        'observacoes' => ['nullable', 'string'],
-      ],
-      [
-        'descricao.required' => 'Preencha o campo Descrição',
-        'observacoes.string' => 'Conteúdo inválido',
-
-      ]
-    );
-
-
-    $material_padrao = AreaAtuacao::create($validated);
-
-    if (!$material_padrao) {
-      return redirect()->back()
-        ->with('error', 'Ocorreu um erro! Revise os dados e tente novamente');
+        return view('painel.areas-atuacao.index', ['areas_atuacao' => $areas_atuacao]);
     }
 
-    return redirect()->route('area-atuacao-index')
-      ->with('success', 'Área de atuação cadastrada com sucesso');
-  }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate(
+            [
+                'descricao' => ['required', 'string'],
+                'observacoes' => ['nullable', 'string'],
+            ],
+            [
+                'descricao.required' => 'Preencha o campo Descrição',
+                'observacoes.string' => 'Conteúdo inválido',
 
+            ]
+        );
 
-  /**
-   * Update the specified resource in storage.
-   */
-  public function update(Request $request, AreaAtuacao $areaAtuacao)
-  {
-    $validated = $request->validate(
-      [
-        'descricao' => ['required', 'string'],
-        'observacoes' => ['nullable', 'string'],
-      ],
-      [
-        'descricao.required' => 'Preencha o campo Descrição',
-        'observacoes.string' => 'Conteúdo inválido',
+        $material_padrao = AreaAtuacao::create($validated);
 
-      ]
-    );
+        if (! $material_padrao) {
+            return redirect()->back()
+                ->with('error', 'Ocorreu um erro! Revise os dados e tente novamente');
+        }
 
-    $areaAtuacao->update($validated);
+        return redirect()->route('area-atuacao-index')
+            ->with('success', 'Área de atuação cadastrada com sucesso');
+    }
 
-    return redirect()->route('area-atuacao-index')
-      ->with('success', 'Área de atuação atualizado com sucesso');
-  }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AreaAtuacao $areaAtuacao)
+    {
+        $validated = $request->validate(
+            [
+                'descricao' => ['required', 'string'],
+                'observacoes' => ['nullable', 'string'],
+            ],
+            [
+                'descricao.required' => 'Preencha o campo Descrição',
+                'observacoes.string' => 'Conteúdo inválido',
 
-  /**
-   * Remove the specified resource from storage.
-   */
-  public function destroy(AreaAtuacao $areaAtuacao)
-  {
-    $areaAtuacao->delete();
+            ]
+        );
 
-    return redirect()->route('area-atuacao-index')
-      ->with('warning', 'Área de atuação removida com sucesso');
-  }
+        $areaAtuacao->update($validated);
+
+        return redirect()->route('area-atuacao-index')
+            ->with('success', 'Área de atuação atualizado com sucesso');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AreaAtuacao $areaAtuacao)
+    {
+        $areaAtuacao->delete();
+
+        return redirect()->route('area-atuacao-index')
+            ->with('warning', 'Área de atuação removida com sucesso');
+    }
 }
